d3ace6303e9292552fa23d21bfc82b7334d1eae2
JamxsBart <140333958+JamxsBart@users.noreply.github.com>
2024-04-22 13:51:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function scrollToModels1() {
+    var container1 = document.getElementById("container1");
+    container1.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  function scrollToModels2() {
+    var container5 = document.getElementById("container5");
+    container5.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  function scrollToModels3() {
+    var container1 = document.getElementById("container1");
+    container1.scrollIntoView({ behavior: 'smooth' });
+  }
